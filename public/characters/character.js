85f1454b89e11b06b633732cc1f3c634948094a0
Klinton Smith <klinton.jp@gmail.com>
2021-02-08 22:30:30 -0700

    back button

--- public/characters/character.js
+++ public/characters/character.js
@@ -1,5 +1,6 @@
 const charName = location.pathname.substring(location.pathname.lastIndexOf("/") + 1).slice(0, -5);
 
+<<<<<<< HEAD
 function goBack() {
     window.history.back();
   }
@@ -7,6 +8,10 @@ function goBack() {
 document.getElementById("app").innerHTML = `
 <button class="backBtn" onclick="goBack()">Go Back</button>
     <header>
+=======
+document.getElementById("app").innerHTML = `
+<header>
+>>>>>>> d76a61a199387a5b16a7bc188cc9e42e88ff9003
     <h1>${charName}</h1>
     <img class="profilePic" src="../images/${charName}/${charName}-profile.png" />
     </header>
@@ -19,10 +24,18 @@ document.getElementById("app").innerHTML = `
     <p>HP</p>
     <p>Energy Recharge %</p>
     </div>    
+<<<<<<< HEAD
     <img class ="artifactPic box" src="../images/${charName}/${charName}-flower.png" />
     <img class ="artifactPic box" src="../images/${charName}/${charName}-plume.png" />
     <img class ="artifactPic box" src="../images/${charName}/${charName}-sands.png" />
     <img class ="artifactPic box" src="../images/${charName}/${charName}-goblet.png" />
     <img class ="artifactPic box" src="../images/${charName}/${charName}-circlet.png" />
+=======
+    <img class ="artifactPic" src="../images/${charName}/${charName}-flower.png" />
+    <img class ="artifactPic" src="../images/${charName}/${charName}-plume.png" />
+    <img class ="artifactPic" src="../images/${charName}/${charName}-sands.png" />
+    <img class ="artifactPic" src="../images/${charName}/${charName}-goblet.png" />
+    <img class ="artifactPic" src="../images/${charName}/${charName}-circlet.png" />
+>>>>>>> d76a61a199387a5b16a7bc188cc9e42e88ff9003
     </div>
 `;
