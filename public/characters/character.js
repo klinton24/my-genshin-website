395cd9f79867e0fcb9718badd227a7c7e930e50c
Klinton Smith <klinton.jp@gmail.com>
2021-02-08 22:27:15 -0700

    back button

---
+++ public/characters/character.js
@@ -0,0 +1,28 @@
+const charName = location.pathname.substring(location.pathname.lastIndexOf("/") + 1).slice(0, -5);
+
+function goBack() {
+    window.history.back();
+  }
+
+document.getElementById("app").innerHTML = `
+<button class="backBtn" onclick="goBack()">Go Back</button>
+    <header>
+    <h1>${charName}</h1>
+    <img class="profilePic" src="../images/${charName}/${charName}-profile.png" />
+    </header>
+    <div class="contentWrap">
+    <div class="informationWrap">
+    <br><br>
+    <h2>Stats to prioritize:</h2>
+    <p>Main Stat: HP %</p>
+    <p>Attack %</p>
+    <p>HP</p>
+    <p>Energy Recharge %</p>
+    </div>    
+    <img class ="artifactPic box" src="../images/${charName}/${charName}-flower.png" />
+    <img class ="artifactPic box" src="../images/${charName}/${charName}-plume.png" />
+    <img class ="artifactPic box" src="../images/${charName}/${charName}-sands.png" />
+    <img class ="artifactPic box" src="../images/${charName}/${charName}-goblet.png" />
+    <img class ="artifactPic box" src="../images/${charName}/${charName}-circlet.png" />
+    </div>
+`;
