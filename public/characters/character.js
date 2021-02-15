c698b5f2cfd6119627709691e41d62f9a23cb9a0
Klinton Smith <klinton.jp@gmail.com>
2021-02-15 06:23:08 -0700

    Seems to be right, need to call function

--- public/characters/character.js
+++ public/characters/character.js
@@ -10,45 +10,26 @@ function goBack() {
     window.history.back();
 }
 
-async function getCharData() {
+
+(async function getCharData() {
   let response = await fetch("./charData.json");
   let data = await response.json();
   return data;
-}
-
-getCharData().then((charData) => {
-  for (var i = 0; i < charData.length; i++) {
-    if (charData[i].cName == "diona") {
-      console.log(charData[i].mainStat);
-      // instead of console.log you would do what you want with the data besides returning
-      charMainStat = charData[i].mainStat;
-      console.log(charMainStat); // This returns the value I want
-    }
-  }
 });
-console.log(charMainStat); //This returns undefined? seems like a scope problem that i can't fix?
 
-/*
-function charMainStat() {
-  let newValue = ''
-  fetch("./charData.json")
-  .then(response => {
-      return response.json();
-  })
-  .then(charData => {
-      for (var i = 0; i < charData.length; i++) {
-          if (charData[i].cName == charName) {
-              newValue = charData[i].mainStat;
-              console.log(newValue);
-          }
+(async function wtf()  {
+  const charData = await getCharData();
+  let charMainStat;
+  for (var i = 0; i < charData.length; i++) {
+      if (charData[i].cName == "diona") {
+        console.log(charData[i].mainStat);
+        // instead of console.log you would do what you want with the data besides returning
+        charMainStat = charData[i].mainStat;
+        console.log(charMainStat); // This returns the value I want
       }
-  });
-  console.log(newValue);
-  return newValue;
-}
-
-console.log(charMainStat());
-*/
+  }
+  console.log(charMainStat); //This returns undefined?
+});
 
 
 document.getElementById("app").innerHTML = `
